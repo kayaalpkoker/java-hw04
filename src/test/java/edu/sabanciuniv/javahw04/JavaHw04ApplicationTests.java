aad9ef13ee019151814bac3ef3267dc01db1c0ae
Kaya Alp Köker <kayaalp.koker@gmail.com>
2023-03-06 00:16:50 +0300

    First commit

---
+++ src/test/java/edu/sabanciuniv/javahw04/JavaHw04ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.sabanciuniv.javahw04;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaHw04ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
